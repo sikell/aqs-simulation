0612f4627e9ca5864bcc65f2974e846ebaf92546
Tim Denzel <ti261den@htwg-konstanz.de>
2025-02-13 13:17:02 +0100

    implemented two additional Algorithms

---
+++ aqs-taxi-algorithm/src/main/java/de/sikeller/aqs/taxi/algorithm/TaxiAlgorithmFillRandomSeats.java
@@ -0,0 +1,42 @@
+package de.sikeller.aqs.taxi.algorithm;
+
+import de.sikeller.aqs.model.*;
+import de.sikeller.aqs.model.AlgorithmResult;
+import java.util.Map;
+import java.util.Set;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class TaxiAlgorithmFillRandomSeats extends AbstractTaxiAlgorithm implements TaxiAlgorithm {
+
+  private Map<String, Integer> parameters;
+  private final String name = "FillRandomSeats";
+
+  public void setParameters(Map<String, Integer> parameters) {
+    this.parameters = parameters;
+  }
+
+  @Override
+  public SimulationConfiguration getParameters() {
+    return new SimulationConfiguration();
+  }
+
+  @Override
+  public AlgorithmResult nextStep(World world, Set<Client> waitingClients) {
+    var taxiCandidates = getTaxisWithCapacity(world);
+    if (taxiCandidates.isEmpty()) return stop("No taxis with capacity found.");
+    var nextTaxi = taxiCandidates.iterator().next();
+    var capacity = Math.random() * nextTaxi.getCurrentCapacity() + 1;
+    var nearestClients = EntityUtils.sortByNearest(nextTaxi.getPosition(), waitingClients);
+
+    for (int taxiSeat = 0; taxiSeat < capacity && taxiSeat < nearestClients.size(); taxiSeat++) {
+      var nextClient = nearestClients.get(taxiSeat).v1();
+      planClientForTaxi(nextTaxi, nextClient, TargetList.mergeOrders);
+      log.debug("Taxi {} with {} seats to fill plan client {}", nextTaxi.getName(), capacity, nextClient.getName());
+    }
+
+    return ok();
+  }
+}
---
+++ aqs-taxi-algorithm/src/main/java/de/sikeller/aqs/taxi/algorithm/TaxiAlgorithmGroupByProximity.java
@@ -0,0 +1,58 @@
+package de.sikeller.aqs.taxi.algorithm;
+
+import de.sikeller.aqs.model.*;
+import de.sikeller.aqs.model.AlgorithmResult;
+import java.util.Map;
+import java.util.Set;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class TaxiAlgorithmGroupByProximity extends AbstractTaxiAlgorithm implements TaxiAlgorithm {
+
+  private Map<String, Integer> parameters;
+  private final String name = "GroupByProximity";
+
+  public void setParameters(Map<String, Integer> parameters) {
+    this.parameters = parameters;
+  }
+
+  @Override
+  public SimulationConfiguration getParameters() {
+    return new SimulationConfiguration("DetectionRadius");
+  }
+
+  @Override
+  public AlgorithmResult nextStep(World world, Set<Client> waitingClients) {
+    var nextClient = waitingClients.iterator().next();
+
+    var taxiCandidates = getEmptyTaxis(world);
+    if (taxiCandidates.isEmpty()) return stop("No taxis with capacity found.");
+
+    var nearestTaxiWithCapacity =
+        EntityUtils.findNearestNeighbor(nextClient.getPosition(), taxiCandidates);
+    if (nearestTaxiWithCapacity == null) return fail("No nearest taxi found.");
+
+    Taxi nearestTaxi = nearestTaxiWithCapacity.v1();
+
+    planClientForTaxi(nearestTaxi, nextClient, TargetList.mergeOrders);
+    log.debug("Taxi {} plan client {}", nearestTaxi.getName(), nextClient.getName());
+
+    // search for others with same target and start
+    var clientsOrderedBySameTarget =
+        EntityUtils.sortByNearest(nextClient.getTarget(), waitingClients, Entity::getTarget);
+    for (Tuple<Client, Double> otherClient : clientsOrderedBySameTarget) {
+      if (!nearestTaxi.hasCapacity()) {
+        break;
+      }
+      if (!otherClient.v1().equals(nextClient)
+          && otherClient.v2() < parameters.get("DetectionRadius")) {
+        Client client = otherClient.v1();
+        planClientForTaxi(nearestTaxi, client, TargetList.mergeOrders);
+      }
+    }
+
+    return ok();
+  }
+}
