64170d4aca1c7050baeb46802bd9a172fed9bd49
Simon Keller <skeller@htwg-konstanz.de>
2025-05-09 23:27:34 +0200

    some options to reduce drawn objects

--- aqs-visualization/src/main/java/de/sikeller/aqs/visualization/drawing/VisualizationProperties.java
+++ aqs-visualization/src/main/java/de/sikeller/aqs/visualization/drawing/VisualizationProperties.java
@@ -9,7 +9,9 @@ public class VisualizationProperties
         BackgroundDrawing.BackgroundDrawingProperties {
   private boolean showClientPaths = true;
   private boolean showClientNames = true;
+  private boolean showClientPositions = true;
   private boolean showTaxiPaths = true;
+  private boolean showTaxiPositions = true;
   private boolean showTaxiNames = true;
   private boolean showFinishedClients = false;
   private boolean showScale = true;
