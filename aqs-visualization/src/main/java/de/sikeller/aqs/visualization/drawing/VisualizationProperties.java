9967c1d03d7d81ec52cae05b916af0fe86b328fa
Simon Keller <skeller@htwg-konstanz.de>
2025-05-14 15:18:46 +0200

    change some default visu properties

--- aqs-visualization/src/main/java/de/sikeller/aqs/visualization/drawing/VisualizationProperties.java
+++ aqs-visualization/src/main/java/de/sikeller/aqs/visualization/drawing/VisualizationProperties.java
@@ -7,14 +7,14 @@ public class VisualizationProperties
     implements TaxiDrawing.TaxiDrawingProperties,
         ClientDrawing.ClientDrawingProperties,
         BackgroundDrawing.BackgroundDrawingProperties {
-  private boolean showClientPaths = true;
-  private boolean showClientNames = true;
+  private boolean showClientPaths = false;
+  private boolean showClientNames = false;
   private boolean showClientPositions = true;
-  private boolean showTaxiPaths = true;
+  private boolean showTaxiPaths = false;
   private boolean showTaxiPositions = true;
-  private boolean showTaxiNames = true;
+  private boolean showTaxiNames = false;
   private boolean showFinishedClients = false;
   private boolean showScale = true;
   private boolean showTime = true;
-  private int scale = 6;
+  private int scale = 4;
 }
